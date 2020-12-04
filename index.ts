921eb532521cfcb738935e243e120737b854d6b8
amicarel <antonio.amicarella@gmail.com>
2020-12-05 00:22:29 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,7 +1,24 @@
-import {InMemoryAuthService} from './auth/auth.inmemory.service'
+import { combineLatest } from "rxjs";
+import { filter, tap } from "rxjs/operators";
+import { InMemoryAuthService } from "./auth/auth.inmemory.service";
 
 // vaffanculo
 // culone
 
-const authService = new InMemoryAuthService()
-authService.login('aa@test.com', '12345678')
+const authService = new InMemoryAuthService();
+function login() {
+  authService.login("aa@test.com", "12345678");
+  let authStatus$ = authService.authStatus$
+  let currentUser$ = authService.currentUser$
+  combineLatest([authStatus$, currentUser$]) 
+  .pipe(
+    filter(([authStatus, user]) => 
+      authStatus.isAuthenticated && user?._id !== ''
+    ),
+    tap(([authStatus, user]) => {
+      console.log('authstatus è ' + JSON.stringify(authStatus) + '\n user è ' + JSON.stringify(user))
+    })
+  )
+  .subscribe()
+}
+login()
